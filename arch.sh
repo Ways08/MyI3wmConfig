2eaeeff0679efdeaf2acea2a41f6e1a3f0c903b7
Ways08 <68125263+Ways08@users.noreply.github.com>
2021-09-11 17:06:18 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #arch apps installation
-sudo pacman -Suy nvidia-dkms curl nvidia-utils rofi i3 i3-gaps xorg zsh xorg-xdm dmenu i3status i3lock ttf-dejavu alacritty
+sudo pacman -Suy nvidia-dkms curl nvidia-utils rofi i3 polkit lxsession i3-gaps xorg zsh xorg-xdm dmenu i3status i3lock ttf-dejavu alacritty dhcpcd neofetch
+echo “exec i3” > ~/.xsession
+chmod +x ~/.xsession
+sudo systemctl enable xdm.service
+
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R tecmint:tecmint ./yay-git
@@ -12,13 +16,27 @@ cd ~/
 yay -S polybar
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+#removing
+rm -rf ~/.config/i3
+rm -rf ~/.config/gtk*
+rm -rf ~/.config/nitrogen
+rm -rf ~/.config/rofi
+rm -rf ~/.config/polybar
+rm -rf ~/.config/alacritty
+
 #copying
 mkdir -p ~/.config
-cp ~/dots/i3 ~/.config/
-cp ~/dots/gtk-* ~/.config/
-cp ~/dots/nitrogen ~/.config/
-cp ~/dots/polybar ~/.config/
-cp ~/dots/rofi ~/.config/
-cp ~/dots/alacritty ~/.config/
+cp .zshrc ~/
+cp ~/MyI3wmConfig/i3 ~/.config/
+cp ~/MyI3wmConfig/gtk-* ~/.config/
+cp ~/MyI3wmConfig/nitrogen ~/.config/
+cp ~/MyI3wmConfig/polybar ~/.config/
+cp ~/MyI3wmConfig/rofi ~/.config/
+cp ~/MyI3wmConfig/alacritty ~/.config/
 mkdir -p ~/Pictures
-cp ~/dots/Pictures/* ~/Pictures/
+cp ~/MyI3wmConfig/Pictures/* ~/Pictures/
+
+#cust 
+clear && neofetch && printf "\n\n\n\nDONE"
+sleep 5
+reboot
